75a5743c4c25f81ac0aa0786d7838d42b40ebdd3
TolisSth <tolishalis@gmail.com>
2024-03-02 18:00:48 +0200

    Implemented deleting mechanism

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 // Author: Apostolos Chalis 2024 
 // Rust Remove is a rm clone written in Rust as a method to learn the language. 
+use std::fs; 
+use std::io; 
 use std::env; 
 use regex::Regex; 
 
@@ -10,6 +12,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let flag = &args[1]; // supports one CLI argument right now 
 
+    // Options should be first because the REGEX expression sucks at understanding that -v and -h
+    // are not file names 
     if flag == "-v"{
         println!("Version 0.1.0 (Prototype)\nAuthor: Apostolos Chalis 2024");
     }
@@ -17,10 +21,9 @@ fn main() {
         println!("rrm [FILE PATH] or rrm [OPTION]\nOptions:\n-v displays version info.\n-h displays this message.")
     }
     else if file_path_regex.is_match(flag){
-        println!("About to remove file: {flag}");
+        let _ = fs::remove_file(flag);
     }
     else{
         println!("Error: Unsupported flag!");
     }
- 
 }
